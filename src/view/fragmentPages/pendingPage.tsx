39f017157aa3ec15b421f2364f937e461f94957a
developertroll <taegyu1500@gmail.com>
2024-05-10 16:54:54 +0900

    파이어베이스 함수 리팩토링

--- src/view/fragmentPages/pendingPage.tsx
+++ src/view/fragmentPages/pendingPage.tsx
@@ -1,7 +1,7 @@
-import PendingLayout from "@/layouts/pendingLayout";
 import { PendingContext } from "@/context/PendingContext";
 import React, { useContext } from "react";
 import { Separator } from "@/components/ui/separator";
+import PendingTableLayout from "@/layouts/pendingTableLayout";
 
 export default function PendingPage() {
   const context = useContext(PendingContext);
@@ -10,9 +10,11 @@ export default function PendingPage() {
 
   return (
     <React.Fragment>
-      {context.productList.map((product, index) => (
-        <PendingLayout key={product.id} product={product} order={index + 1} />
-      ))}
+      {context.productList.length > 0 ? (
+        <PendingTableLayout />
+      ) : (
+        <div>상품이 없습니다</div>
+      )}
       <Separator />
       {context.calcPrice() > 0 && <div>총 가격:{context.calcPrice()}원</div>}
     </React.Fragment>
