27b8d220fdcf4f5467096b4a63749f2b5d95cbdb
developertroll <taegyu1500@gmail.com>
2024-05-18 17:24:58 +0900

    develop: 구글 로그인 구현 및 헤더 문제 해결

--- src/layouts/headerLayout.tsx
+++ src/layouts/headerLayout.tsx
@@ -1,19 +1,29 @@
 import Titlelayout from "./titleLayout";
 import { Input } from "@/components/ui/input";
-import { useContext } from "react";
+import { useContext, useState, useEffect } from "react";
 import { ModalContext } from "@/context/ModalContext";
 import { auth } from "@/firebase";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import { Button } from "@/components/ui/button";
+import { onAuthStateChanged } from "firebase/auth";
 
 export default function HeaderLayout() {
+  const [currentUser, setCurrentUser] = useState(auth.currentUser);
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user);
+    });
+
+    return () => unsubscribe();
+  }, []);
   const context = useContext(ModalContext);
   const Navigate = useNavigate();
+  const location = useLocation();
   const openModal = () => {
     if (!context)
       throw new Error("useModal must be used within a ModalProvider");
     console.log("openModal");
-    context.openModal("PendingPage");
+    context.openModal("PendingPage", "alert");
   };
 
   return (
@@ -26,7 +36,7 @@ export default function HeaderLayout() {
               상품
             </Button>
           </div>
-          {!auth.currentUser && (
+          {!currentUser && (
             <div className="flex gap-4">
               <Button
                 className="btn"
@@ -46,7 +56,7 @@ export default function HeaderLayout() {
               </Button>
             </div>
           )}
-          {auth.currentUser && (
+          {currentUser && (
             <div className="flex gap-4">
               <Button className="btn" onClick={openModal}>
                 장바구니
@@ -59,7 +69,9 @@ export default function HeaderLayout() {
                 onClick={() => {
                   auth.signOut();
                   alert("로그아웃 되었습니다.");
-                  Navigate("/");
+                  if (location.pathname !== "/") {
+                    Navigate("/");
+                  }
                 }}
               >
                 로그아웃
